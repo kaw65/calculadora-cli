c9bb0fc5817a206f2ae0ecf8489cf074d722c199
Kaw68 <92464308+kaw65@users.noreply.github.com>
2022-04-14 19:33:17 -0300

    Add files via upload

---
+++ calculadora.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+int main(int argc, char *argv[])
+{
+ int input = input;
+ int input1 = input1;
+ std::cin >> input;
+ std::cin >> input1;
+ if (input != char()) {
+ if (input1 != char()){
+ std::cout << 
+ input
+<< "+"
+ << input1 << "=" << input+input1<<std::endl;
+ }
+ else {
+ std::cout << "coloque numeros!";
+ }
+ }
+ else {
+  std::cout << "coloque numeros!";
+ }
+}
